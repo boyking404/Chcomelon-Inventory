a5bca8723926d91c717cce8b24754e42b4e70a1c
SUJAY DEY <iphoneindiatoday@gmail.com>
2024-09-09 23:32:47 +0530

    Deploy

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://bharatse-inventory.vercel.app"],
+    origin: ["https://bharatse-inventory.vercel.app"],
     credentials: true,
   })
 );
