c6350f08cfd5077f24af1e220a541cec04a3ec99
SUJAY DEY <iphoneindiatoday@gmail.com>
2024-09-10 20:54:37 +0530

    Complete

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(
   cors({
-    origin: ["https://bharatse-inventory.vercel.app"],
+    origin: ["http://localhost:3000", "https://pinvent-app.vercel.app"],
     credentials: true,
   })
 );
